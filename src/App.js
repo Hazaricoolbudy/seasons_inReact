4b51028d5339fcc39819bcf89e24e11babafba33
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-06-11 13:53:06 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 // import logo from './logo.svg';
 import "./App.css";
-// import Seasons from './com   ponents/Seasons';
+import Seasons from './components/Seasons';
 import React from "react";
 
-// function App() {
-//   window.navigator.geolocation.getCurrentPosition((position)=>{console.log(position)},err=>console.log(err));
-//   return (
-//     <div className="App">
 
-//     <Seasons/>
-
-//     </div>
-//   );
-// }
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errMassage: "" };
+ 
+  state = { lat: null, errMassage: "" };
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
       (err) => this.setState({ errMassage: err.message })
     );
+   
   }
-
+  
   render() {
+    
     if (this.state.errMassage && !this.state.lat) {
       return <div> Error :{this.state.errMassage}</div>;
     }
     if (!this.state.errMassage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <Seasons lat={this.state.lat}/>
     }
 
     return (
--- src/components/Seasons.js
+++ src/components/Seasons.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Seasons = () => {
+
+const getSeasons =(lat,month)=>{
+  if(month>2 && month<9){
+     return lat>0? 'summer':'winter';
+  
+  }
+  else{
+   return lat> 0 ?'winter':'summer';   
+  }
+
+}
+
+const Seasons = (props) => {
+ const seasons=getSeasons(props.lat, new Date().getMonth());
+ const text=seasons==='winter'?'Burr , it is chilly':'Lets hit the beach'
+ console.log(seasons);
   return (
     <div>
-      Seasons display
+    <h1>{text}</h1>
     </div>
   )
 }
